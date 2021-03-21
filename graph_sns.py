a66b4f4c3343b29b8944d9e748a6870731d1a4c8
yuta_uemura <uemurayuta0817@gmail.com>
2021-03-21 15:56:38 +0900

    add graph_sns.py

--- graph_sns.py
+++ graph_sns.py
@@ -4,5 +4,5 @@ from matplotlib import pyplot as plt
 import numpy
 
 df = pd.read_csv(r"C:\Users\uemur\OneDrive\labs\report\mine\graduateThesis\word\lrp_time2.csv", header=0, index_col=0)
-sns.catplot(data=df, x="stage", y="time", kind="box", hue="treatment")
+sns.catplot(data=df, x="stage", y="time (min)", kind="box", hue="treatment", dodge=True)
 plt.show()
